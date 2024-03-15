4221543201cb0270986dbd60b94ae9bb2d6b6c80
mouhamad ataa al daghstani <ataa_co@hotmail.com>
2024-03-16 02:15:36 +0300

    Add first commit

---
+++ HostServices/BackGroundService.cs
@@ -0,0 +1,18 @@
+﻿namespace HostServices;
+internal sealed class BackGroundService(IServiceScopeFactory serviceScopeFactory) : BackgroundService
+{
+    private IServiceScopeFactory _serviceScopeFactory { get; } = serviceScopeFactory;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var peridoicTimer = new PeriodicTimer(TimeSpan.FromSeconds(5));
+
+        while (!stoppingToken.IsCancellationRequested 
+            && await peridoicTimer.WaitForNextTickAsync())
+        {
+            using AsyncServiceScope scope = _serviceScopeFactory.CreateAsyncScope();
+            ILogger? logger = scope.ServiceProvider.GetService<ILogger<BackGroundService>>();
+            logger?.LogInformation("Hello from background service");
+        }
+    }
+}
---
+++ HostServices/HostServiceExample.cs
@@ -0,0 +1,41 @@
+﻿namespace HostServices
+{
+    internal sealed class HostServiceExample : IHostedLifecycleService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Start host service");
+            return Task.CompletedTask;
+        }
+
+        public Task StartedAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Started host service");
+            return Task.CompletedTask;
+        }
+
+        public Task StartingAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Starting host service");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Stop host service");
+            return Task.CompletedTask;
+        }
+
+        public Task StoppedAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Stopped host service");
+            return Task.CompletedTask;
+        }
+
+        public Task StoppingAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Stopping host service");
+            return Task.CompletedTask;
+        }
+    }
+}
